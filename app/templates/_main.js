01b54d23359f2be9e82a9b82e9f5a9a24aa5e204
Doug Macklin <doug.macklin@gmail.com>
2016-02-23 12:03:52 -0800

    removed browserify-shim as it is no longer needed

--- app/templates/_main.js
+++ app/templates/_main.js
@@ -1,4 +1,4 @@
-var $          = require('jquery');
+global.$       = global.jQuery = require('jquery');
 var foundation = <% if (props.foundationVersion === 'Foundation 6') { %>require('../../node_modules/foundation-sites/dist/foundation.js');<% } else { %>require('foundation');<% } %>
 
 $(document).foundation();
