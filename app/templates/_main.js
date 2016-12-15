c6c14fbc9c0cd0432d3523fc34e41ab9b7ce3017
Doug Macklin <doug.macklin@gmail.com>
2016-12-15 14:25:48 -0800

    fix js location for 6.3.0

--- app/templates/_main.js
+++ app/templates/_main.js
@@ -1,4 +1,4 @@
 global.$       = global.jQuery = require('jquery');
-var foundation = <% if (props.foundationVersion === 'Foundation 6') { %>require('../../node_modules/foundation-sites/dist/foundation.js');<% } else { %>require('foundation');<% } %>
+var foundation = <% if (props.foundationVersion === 'Foundation 6') { %>require('../../node_modules/foundation-sites/dist/js/foundation.js');<% } else { %>require('foundation');<% } %>
 
 $(document).foundation();
